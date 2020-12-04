8238059467c2f5a61ad91da250ec9bcdcdad465c
Charles King <charles.d.king.ii@gmail.com>
2020-12-04 16:31:35 -0500

    Updates to server listener

--- server.js
+++ server.js
@@ -14,11 +14,22 @@ app.get('/', function(req, res) {
     });
 });
 
+app.get('/file', function(req, res){
+    fs.readFile('index.html', function(err, buffer){
+        var html = buffer.toString();
+        res.setHeader('Content-Type', 'text/html');
+        res.send(html);
+    });
+});
+
 app.get('/data', function(req, res) {
     res.send(jsonData);
 });
 
-app.listen(8888);
+var port = 8888;
+app.listen(8888, function(){
+    console.log('Server listening on port: ' + port);
+});
 // TODO: create a basic server with express
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
