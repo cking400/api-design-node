f050b9b5bd827d8f9996b34e86139b42231f4670
Charles King <charles.d.king.ii@gmail.com>
2020-12-15 14:34:11 -0500

    Home work solution

--- server/lions.js
+++ server/lions.js
@@ -1,4 +1,5 @@
 var lionRouter = require('express').Router();
+var _ = require('lodash');
 
 var lions = [];
 var id = 0;
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var _ = require('lodash');
 var morgan = require('morgan');
 
 var lionRouter = require('./lions');
-var tigerRouter = require('./tigers');
+var tigerRouter = require('./tigers'); 
 
 app.use(morgan('dev'))
 app.use(express.static('client'));
@@ -16,6 +16,7 @@ app.use(bodyParser.json());
 // this is called mounting. when ever a req comes in for
 // '/lion' we want to use this router
 app.use('/lions', lionRouter);
+app.use('/tigers', tigerRouter);
 
 app.use(function(err, req, res, next) {
   if (err) {
